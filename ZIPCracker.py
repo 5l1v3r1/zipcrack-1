41181affb599569f782a098eba273b511f9b7d03
Ed <Ed@eds-macbook-air.home>
2019-05-15 13:11:54 +0100

    Fix for hanging on OSX

--- ZIPCracker.py
+++ ZIPCracker.py
@@ -3,30 +3,36 @@ import zipfile
 import optparse
 from threading import Thread
 
-def extractFile(zFile, password):
-    try:
-        zFile.extractall(pwd=password)
-        print '[+] Brute Force Successful: ' + password + '\n'
-        return password
-    except:
-        return
+
 def main():
     parser = optparse.OptionParser('usage: zipcracker.py ' + '-f <zipfile> -w <wordlist>')
-    parser.add_option('-f', dest='zname',type='string',help='specify zip file')
-    parser.add_option('-w', dest='wname',type='string',help='specify wordlist file')
+    parser.add_option('-f', dest='zipf',type='string',help='specify zip file')
+    parser.add_option('-w', dest='wordf',type='string',help='specify wordlist file')
     (options,args) = parser.parse_args()
-    if (options.zname == None) | (options.wname == None):
-        print parser.usage
-        exit(0)
+    if (options.zipf == None) | (options.wordf == None):
+        print(parser.usage)
     else:
-        zname = options.zname
-        wname = options.wname
-    zFile = zipfile.ZipFile(zname)
-    passFile = open(wname)
-    for line in passFile.readlines():
+        zipf = options.zipf
+        wordf = options.wordf
+    zipf = zipfile.ZipFile(zipf)
+    wordf = open(wordf)
+    for line in wordf.readlines():
         password = line.strip('\n')
-        t = Thread(target=extractFile, args=(zFile, password))
+        t = Thread(target=extractFile, args=(zipf, password))
         t.start()
+
+def extractFile(zipf, password):
+    try:
+        zipf.extractall(pwd=password)
+        print('\n[+] Brute Force Successful: ' + password)
+        return password
+    except:
+        space = len(password)
+        space = ' ' * space
+        sys.stdout.write('[+] Trying ' + password + space + '\r')
+        sys.stdout.flush()
+        return
+
 
 if __name__ == '__main__':
     main()
